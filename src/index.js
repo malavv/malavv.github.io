a8a439adcf50a0b4fe412c69dc9e306264f08178
Maxime Lavigne (malavv) <duguigne@gmail.com>
2018-10-08 18:59:47 -0400

    Content and Footer being loaded async
    Everything but the front page is now instantly loaded.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,28 @@ import _ from 'lodash';
 
 import './style.scss';
 
-import content from './content.html';
+import title from './title.html';
 
-document.body.innerHTML = content;
+document.body.innerHTML = title;
+
+document.addEventListener("DOMContentLoaded", () => {
+  import('./content.html')
+      .then(m => m.default)
+      .then(parseHTML)
+      .then(obj => {
+        document.querySelector('div.content').appendChild(obj);
+      });
+
+  import('./footer.html')
+      .then(m => m.default)
+      .then(parseHTML)
+      .then(obj => {
+        document.body.appendChild(obj);
+      });
+});
+
+function parseHTML(html) {
+  let tmpl = document.createElement('template');
+  tmpl.innerHTML = html;
+  return tmpl.content;
+}
